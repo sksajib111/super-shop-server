8baddf9677fb65dcf921b95de6e4969b42b29ca8
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2023-12-05 23:51:07 +0600

    mongoose add

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@pc-tech-bd.hievdt4.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pc-tech-bd.hievdt4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
