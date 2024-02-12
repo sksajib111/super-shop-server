88f2909b692e41fd5846a041f5dd792f2c72c808
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-02-12 23:50:57 +0600

    get carts items depends by email

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -12,8 +12,7 @@ app.get("/", (req, res) => {
   res.send("Super Shop Server is running");
 });
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pc-tech-bd.hievdt4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,40 +33,42 @@ async function run() {
     const reviewCollection = client.db("superShopDB").collection("reviews");
     const cartCollection = client.db("superShopDB").collection("carts");
 
-    app.get('/menu', async(req, res) =>{
+    app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
-    app.get('/reviews', async(req, res) =>{
+    app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
 
     // cart collection
-    app.post('/carts', async(req,res)=>{
+    app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
-    })
+    });
 
-    app.get('/carts', async(req, res)=>{
-      const result = await cartCollection.find().toArray();
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Super Shop Server is Running on port ${port}`);
