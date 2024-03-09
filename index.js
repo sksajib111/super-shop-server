fa074b61fc4bd86702721e0b7e8f8b0eb10a2137
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2024-03-09 22:59:33 +0600

    google login user chacking function implement

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
     // users related api
     app.post('/users', async (req, res)=>{
       const user = req.body;
+      // insert email if user dose'n exists:
+      // simple checking
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
